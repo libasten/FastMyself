0bdbc425049099fe0a9951e93f41476d485eb3b5
李成武 <lichengwu@foxmail.com>
2020-11-19 11:52:59 +0800

    修改底部zb链接。

--- template/footer.php
+++ template/footer.php
@@ -1,6 +1,6 @@
 <div class="footer">
     <div class="copyright">{$copyright}</div>
-    <div style="margin-top: 10px;">Powered By {$zblogphp}</div>
+    <div style="margin-top: 10px;">Powered By <a href="https://www.zblogcn.com/" target="_blank">{$zblogphp}</a></div>
 </div>
 <script src="{$host}zb_system/script/jquery-2.2.4.min.js" type="text/javascript"></script>
 <script src="{$host}zb_system/script/zblogphp.js" type="text/javascript"></script>
