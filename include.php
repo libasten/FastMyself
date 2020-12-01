f5b6428ba93df6b5e1bed891cd09cc5928f49640
lichengwu <lichengwu@foxmail.com>
2020-12-02 00:46:07 +0800

    取消缩略图上传图片功能。

--- include.php
+++ include.php
@@ -10,23 +10,15 @@ function ActivePlugin_FastMyself() {
 // 在顶部添加菜单    	    	 	    	 	     
 function FastMyself_AddMenu(&$m){    				        	 		 	  
     global $zbp;       	 	       	  	 	 
-    $m[] = MakeTopMenu("root", 'FastMyself主题设置说明', $zbp->host . "zb_users/theme/FastMyself/editor.php", "", "topmenu_FastMyself");    			         			 	   
+    $m[] = MakeTopMenu("root", 'FastMyself主题设置', $zbp->host . "zb_users/theme/FastMyself/editor.php", "", "topmenu_FastMyself");    			         			 	   
 }
 
 function FastMyself_Filter_Plugin_Edit_Response5(){     		  		       	   		
-    global $zbp, $article;    			  		     				 	  
-    echo "<script type=\"text/javascript\" src=\"{$zbp->host}zb_users/theme/FastMyself/script/lib.upload.js\"></script>";
-	echo '<div class="uploadimg" style="width:80%; height:100%;float:left;border-bottom:1px solid #ddd;margin-bottom:16px;">
-    <label for="editor_content" class="editinputname">封面缩略图地址</label><br>
+    global $article;    			  		     				 	  
+	echo '<div style="width:100%; margin-bottom:15px;">
+    <label for="edtTitle" class="editinputname">封面图URL</label>
     <input name="meta_fdsImage" id="edtTitle" type="text" class="uplod_img" maxwidth="100" value="'.$article->Metas->fdsImage.'" />
-    <strong class="button" style="color: #fff; padding: 6px 18px 6px 18px;margin: 0.5em 0;
-        background: #3a6ea5;border: 1px solid #3399cc;cursor: pointer;display: block; height: 32px;width: 122px;">浏览上传图片</strong>
-    </div>
-    <div style="width:20%;float:right;border-left:1px solid #ddd;padding-left:10px;border-bottom:1px solid #ddd;padding-left:10px;height: 102px;">
-    <label for="editor_content" class="editinputname">封面缩略图</label><br>
-    <a href="'.$article->Metas->fdsImage.'" target="_blank">
-        <img id="preview" src="'.$article->Metas->fdsImage.'" max-width="95%" style="height:76px">
-    </a></div>';
+    </div>';
 }
     		 	      			 			 
 function InstallPlugin_FastMyself() {     	 	 		       	    	
